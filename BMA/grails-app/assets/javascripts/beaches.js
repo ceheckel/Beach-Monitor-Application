d91aa41d7435c76ef0ea2ed5846d10e008774dfa
Brett <bakriz@mtu.edu>
2018-02-03 15:31:27 -0500

    New Parser
    Added a new parser that can request the list from the WI server! No longer using this static test list (its deleted)  May need some testing but looks fine

--- BMA/grails-app/assets/javascripts/beaches.js
+++ BMA/grails-app/assets/javascripts/beaches.js
@@ -2,6 +2,13 @@
  * beaches defines the data used for autocompletion on the beach selection page.
  * The data maps counties to lakes to beaches to sites
  */
+var beaches = [];
+var callback = function (gotten_beaches) {
+    beaches = gotten_beaches;
+};
+beaches_sites_get.run(callback,false);
+
+/*
 
 var beaches = {
     'Adams': {
@@ -621,3 +628,4 @@ var beaches = {
         },
     },
 };
+ */
