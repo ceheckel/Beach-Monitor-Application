4129ec0e74704f49cbce31d7180e7b54353d5f43
Charlie Heckel <ceheckel@c28-0117-12.ad.mtu.edu>
2018-02-06 13:32:25 -0500

    Finalized POST format

--- BMA/grails-app/assets/javascripts/survey_post.js
+++ BMA/grails-app/assets/javascripts/survey_post.js
@@ -1,5 +1,6 @@
 /**
  * Created by cwbaldwi on 10/11/17.
+ * Editted by ceheckel on 02/03/18.
  */
 
 window.survey_post = {};
@@ -8,94 +9,94 @@ survey_post.TEST = false;
 survey_post.POST_URL = "";
 survey_post.LOCALHOST_SURVEYS_URL = "http://localhost:8081/bms/survey";
 survey_post.TOMCAT_SURVEYS_URL = "https://hci-dev.cs.mtu.edu:8117/BMS2/survey"; // TOMCAT URL IS CURRENTLY FOR TESTING SERVER
-
-survey_post.test_survey = {
-    __county: "Test County",
-    __lake: "Test Lake",
-    __beach: "Test Beach",
-    __site: "Test Site",
-    user_id: "asdf",
-    NO_GULLS: 1,
-    NO_GEESE: 2,
-    NO_DOGS: 2,
-    NO_ANIMALS_OTHER: 0,
-    NUM_LOONS: 1,
-    NUM_HERR_GULLS: 1,
-    NUM_RING_GULLS: 1,
-    NUM_CORMORANTS: 1,
-    NUM_LONGTAIL_DUCKS: 1,
-    NUM_SCOTER: 1,
-    NUM_HORN_GREBE: 1,
-    NUM_REDNECKED_GREBE: 1,
-    NUM_FISH: 1,
-    NUM_OTHER: 0,
-    FLOAT_STREET_LITTER: true,
-    FLOAT_FOOD: true,
-    FLOAT_MEDICAL: true,
-    FLOAT_SEWAGE: true,
-    FLOAT_BLDG_MATERIALS: true,
-    FLOAT_FISHING: true,
-    FLOAT_OTHER: false,
-    DEBRIS_STREET_LITTER: true,
-    DEBRIS_FOOD: true,
-    DEBRIS_MEDICAL: true,
-    DEBRIS_SEWAGE: true,
-    DEBRIS_BLDG_MATERIALS: true,
-    DEBRIS_FISHING: true,
-    DEBRIS_HOUSEHOLD: true,
-    DEBRIS_TAR: true,
-    DEBRIS_OIL: true,
-    DEBRIS_OTHER: false,
-    DEBRIS_AMOUNT: "much",
-    NO_IN_WATER: 1,
-    NUM_OUT_OF_WATER: 1,
-    NO_PEOPLE_BOATING: 1,
-    NO_PEOPLE_FISHING: 1,
-    NO_PEOPLE_SURFING: 1,
-    NO_PEOPLE_WINDSURFING: 1,
-    NUM_PEOPLE_DIVING: 1,
-    NO_PEOPLE_CLAMMING: 1,
-    NO_PEOPLE_OTHER: 0,
-    AIR_TEMP: 70,
-    AIR_UNITS: "F",
-    WIND_SPEED: 5,
-    WIND_SPEED_UNITS: "mph",
-    WIND_DIR_DEGREES: 50,
-    WIND_DIR_DESC: "very light",
-    WEATHER_DES: "mostly clear",
-    RAINFALL_LAST_EVENT: "2 hours",
-    RAINFALL: 3,
-    RAINFALL_UNITS: "IN",
-    RAINFALL_STD_DESC: "Not much",
-    WAVE_HEIGHT: 1.2,
-    WAVE_HEIGHT_UNITS: "FT",
-    EST_ACT_FLAG: true,
-    WAVE_DIRECTION: "NNW",
-    WAVE_CONDITIONS: "Not many",
-    CURRENT_SPEED: "10",
-    LONGSHORE_CURRENT_UNITS: "cm/sec",
-    SHORELINE_CURRENT_DIR: "NNW",
-    PH: 7.0,
-    COLOR_CHANGE: false,
-    ODOR_DESCRIPTION: "Smells like a lake alright",
-    AVG_WATER_TEMP: 60,
-    AVG_WATER_TEMP_UNITS: "F",
-    CLARITY_DESC: "Pretty clear, pretty clear",
-    NTU: 1.0,
-    SECCHI_TUBE_CM: 5,
-    ALGAE_NEARSHORE: "yep",
-    ALGAE_ON_BEACH: "yep",
-    ALGAE_TYPE_PERIPHYTON: true,
-    ALGAE_TYPE_GLOBULAR: true,
-    ALGAE_TYPE_FREEFLOATING: true,
-    ALGAE_TYPE_OTHER: false,
-    ALGAE_COLOR_LT_GREEN: true,
-    ALGAE_COLOR_BRGHT_GREEN: true,
-    ALGAE_COLOR_DRK_GREEN: true,
-    ALGAE_COLOR_YELLOW: true,
-    ALGAE_COLOR_BROWN: true,
-    ALGAE_COLOR_OTHER: false
-};
+//
+// survey_post.test_survey = {
+//     __county: "Test County",
+//     __lake: "Test Lake",
+//     __beach: "Test Beach",
+//     __site: "Test Site",
+//     user_id: "asdf",
+//     NO_GULLS: 1,
+//     NO_GEESE: 2,
+//     NO_DOGS: 2,
+//     NO_ANIMALS_OTHER: 0,
+//     NUM_LOONS: 1,
+//     NUM_HERR_GULLS: 1,
+//     NUM_RING_GULLS: 1,
+//     NUM_CORMORANTS: 1,
+//     NUM_LONGTAIL_DUCKS: 1,
+//     NUM_SCOTER: 1,
+//     NUM_HORN_GREBE: 1,
+//     NUM_REDNECKED_GREBE: 1,
+//     NUM_FISH: 1,
+//     NUM_OTHER: 0,
+//     FLOAT_STREET_LITTER: true,
+//     FLOAT_FOOD: true,
+//     FLOAT_MEDICAL: true,
+//     FLOAT_SEWAGE: true,
+//     FLOAT_BLDG_MATERIALS: true,
+//     FLOAT_FISHING: true,
+//     FLOAT_OTHER: false,
+//     DEBRIS_STREET_LITTER: true,
+//     DEBRIS_FOOD: true,
+//     DEBRIS_MEDICAL: true,
+//     DEBRIS_SEWAGE: true,
+//     DEBRIS_BLDG_MATERIALS: true,
+//     DEBRIS_FISHING: true,
+//     DEBRIS_HOUSEHOLD: true,
+//     DEBRIS_TAR: true,
+//     DEBRIS_OIL: true,
+//     DEBRIS_OTHER: false,
+//     DEBRIS_AMOUNT: "much",
+//     NO_IN_WATER: 1,
+//     NUM_OUT_OF_WATER: 1,
+//     NO_PEOPLE_BOATING: 1,
+//     NO_PEOPLE_FISHING: 1,
+//     NO_PEOPLE_SURFING: 1,
+//     NO_PEOPLE_WINDSURFING: 1,
+//     NUM_PEOPLE_DIVING: 1,
+//     NO_PEOPLE_CLAMMING: 1,
+//     NO_PEOPLE_OTHER: 0,
+//     AIR_TEMP: 70,
+//     AIR_UNITS: "F",
+//     WIND_SPEED: 5,
+//     WIND_SPEED_UNITS: "mph",
+//     WIND_DIR_DEGREES: 50,
+//     WIND_DIR_DESC: "very light",
+//     WEATHER_DES: "mostly clear",
+//     RAINFALL_LAST_EVENT: "2 hours",
+//     RAINFALL: 3,
+//     RAINFALL_UNITS: "IN",
+//     RAINFALL_STD_DESC: "Not much",
+//     WAVE_HEIGHT: 1.2,
+//     WAVE_HEIGHT_UNITS: "FT",
+//     EST_ACT_FLAG: true,
+//     WAVE_DIRECTION: "NNW",
+//     WAVE_CONDITIONS: "Not many",
+//     CURRENT_SPEED: "10",
+//     LONGSHORE_CURRENT_UNITS: "cm/sec",
+//     SHORELINE_CURRENT_DIR: "NNW",
+//     PH: 7.0,
+//     COLOR_CHANGE: false,
+//     ODOR_DESCRIPTION: "Smells like a lake alright",
+//     AVG_WATER_TEMP: 60,
+//     AVG_WATER_TEMP_UNITS: "F",
+//     CLARITY_DESC: "Pretty clear, pretty clear",
+//     NTU: 1.0,
+//     SECCHI_TUBE_CM: 5,
+//     ALGAE_NEARSHORE: "yep",
+//     ALGAE_ON_BEACH: "yep",
+//     ALGAE_TYPE_PERIPHYTON: true,
+//     ALGAE_TYPE_GLOBULAR: true,
+//     ALGAE_TYPE_FREEFLOATING: true,
+//     ALGAE_TYPE_OTHER: false,
+//     ALGAE_COLOR_LT_GREEN: true,
+//     ALGAE_COLOR_BRGHT_GREEN: true,
+//     ALGAE_COLOR_DRK_GREEN: true,
+//     ALGAE_COLOR_YELLOW: true,
+//     ALGAE_COLOR_BROWN: true,
+//     ALGAE_COLOR_OTHER: false
+// };
 
 /**
  * Uploads all surveys to the Wi Beach Server
@@ -103,91 +104,44 @@ survey_post.test_survey = {
  */
 survey_post.upload = function(surveys) {
     var promises = [];
-    var successful = [];
-    var unsuccessful = [];
     var toUpload = ""; // all the surveys clumped into one string
-    var surveyAsString = ""; // holder for each survey data after it is stringified
 
-    if (survey_post.TEST) {
+    // Start construction of the survey clump
+    toUpload = "";
+    surveys.forEach(function(survey) {
+        //ensure that water and air temp are null if they ="" upon upload
+        if(survey.AVG_WATER_TEMP == "")
+            survey.AVG_WATER_TEMP = null;
+        if(survey.AIR_TEMP == "")
+            survey.AIR_TEMP = null;
 
-        promises.push($.ajax({
-            type: 'POST',
-            crossDomain: true,
-            contentType: 'application/json; charset=utf-8',
-            url: survey_post.POST_URL,
-            dataType: 'json',
-            data: JSON.stringify(survey_post.test_survey),
-            success: function(response) {
-                successful.push(survey_post.test_survey.id);
-            },
-            error: function(response) {
-                unsuccessful.push(survey_post.test_survey.id);
-            }
-        }));
-    }
-    else {
-        surveys.forEach(function(survey) {
-            //ensure that water and air temp are null if they ="" upon upload
-            if (survey.AVG_WATER_TEMP == "")
-                survey.AVG_WATER_TEMP = null;
-            if (survey.AIR_TEMP == "")
-                survey.AIR_TEMP = null;
-
-            // ensure that survey has been submitted
-            if (survey.submitted) {
-                surveyAsString = JSON.stringify(survey);
-                toUpload = toUpload + surveyAsString;
-            }
-        });
-
-        promises.push($.ajax({
-            type: 'POST',
-            crossDomain: true,
-            contentType: 'application/json; charset=utf-8',
-            url: survey_post.POST_URL,
-            dataType: 'json',
-            data: toUpload,
-            success: function (response) {
-                
-            },
-            error: function (response) {
-                unsuccessful.push(survey);
-                //console.log(getAllFields());
-                //console.log(response);
-            }
-        }));
-    }
-
-    Promise.all(promises).then(function() {
-
-        var alertString = "The following surveys have successfully been uploaded to the server:\n";
-
-        successful.forEach(function(survey) {
-            alertString += "  " + survey.__beach + " survey created by User " + survey.user_id + " on " + survey.date + "\n";
-        });
-
-        if (unsuccessful.length !== 0) {
-            alertString += "\n";
-            alertString += "NOTE: The following surveys were NOT uploaded to the server:\n";
-
-            unsuccessful.forEach(function(survey) {
-                alertString += "  " + survey.__beach + " survey created by User " + survey.user_id + " on " + survey.date + "\n";
-            });
-
-            alertString += "\n";
-            alertString += "Please make sure you having a working network connection and are not attempting to upload surveys that have already been uploaded.";
+        //ensure only submitted surveys get uploaded
+        if (survey.submitted) { // upload only submitted surveys
+            toUpload = toUpload + JSON.stringify(survey) + ","; // stringify survey
         }
-
-        alertString += "\n";
-        alertString += "NOTE: The surveys will remain on your device until you delete them, but you cannot reupload surveys that have already been successfully been uploaded.";
-
-        alert(alertString);
-
-        // Update the beaches/sites lists while we can connect to the server
-        var callback = function (gotten_beaches) {
-            beaches = gotten_beaches;
-        };
-
-        window.beaches_sites_get.run(callback, false);
     });
+    toUpload = toUpload.substr(0,toUpload.length-1) + ""; // removes the last comma
+    console.log(toUpload);
+
+    // POST the clump
+    promises.push($.ajax({
+        type: 'POST',
+        crossDomain: true,
+        contentType: 'application/json; charset=utf-8',
+        url: survey_post.POST_URL,
+        dataType: 'json',
+        data: toUpload,
+        success: function (response) { alert("success"); },
+        error: function (response) { alert("no success:\n" + response); }
+    }));
+
+    Promise.all(promises).then(function() { /* waiting for promises to return */ });
+
+    // Update the beaches/sites lists while we can connect to the server
+    var callback = function (gotten_beaches) {
+        beaches = gotten_beaches;
+    };
+
+    // GET updated info from server
+    window.beaches_sites_get.run(callback, false);
 };
