b522d3816f412a2698a0f238919c905dcd58ddf3
Brett <bakriz@mtu.edu>
2018-04-07 13:30:27 -0400

    Added dynamic Wind Dir Disc
    Wind Disc (theoretically) should update based on wind dir degrees now

--- BMA/grails-app/assets/javascripts/change.js
+++ BMA/grails-app/assets/javascripts/change.js
@@ -103,6 +103,55 @@ function RainfallChange(){
 }
 
 /**
+ * Set the value of Wind Disc based
+ * on the wind degrees input
+ */
+function AlterWindDirDesc() {
+    var cur = $("#WIND_DIR_DEGREES").val();
+    // '', 'Calm', 'Variable','N','NE','E','SE','S','SW','W','NW'
+    // "(0*3[0-5][0-9])|(0*[1-2][0-9][0-9])|(0*[1-9][0-9])|(0*[1-9])|0*"
+    var v = 45/2.0;
+    var N   = cur >= 360-v  ||   cur < v;
+    var NE  = cur >= v      ||   cur < 45+v;
+    var E   = cur >= 45+v  ||   cur < 90+v;
+    var SE  = cur >= 90+v  ||   cur < 180-v;
+    var S   = cur >= 180-v  ||   cur < 180+v;
+    var SW  = cur >= 180+v  ||   cur < 11.25;
+    var W   = cur >= 348.75  ||   cur < 11.25;
+    var NW  = cur >= 348.75  ||   cur < 11.25;
+
+    var res;    // RESULTS
+
+    // Alter WIND_DIR_DESC
+    if ( cur == null || cur == '') {
+        // There is nothing
+        res = 'Calm' // '' and 'Variable' shouldn't be sent
+    } else if (N) {
+        res = 'N';
+    } else if (NE) {
+        res = 'NE';
+    } else if (E) {
+        res = 'E';
+    } else if (SE) {
+        res = 'SE';
+    } else if (S) {
+        res = 'SW';
+    } else if (SW) {
+        res = 'SW';
+    } else if (W) {
+        res = 'W';
+    } else if (NW) {
+        res = 'NW';
+    } else {
+        // This shouldnt happen!!
+        res = 'Variable';
+    }
+
+    // Conform degrees to cardinal directions
+    $("#WIND_DIR_DESC").val(res);
+} // WIND_DIR_DEGREES
+
+/**
  * Setup and removal of the description field for
  * odor based on its html attributes
  */
