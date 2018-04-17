96e4f9776f9ecf144c33c5ad68e3591e9da7ce43
Brett <bakriz@mtu.edu>
2018-04-17 01:16:06 -0400

    Finalized CorrelatedTextQuestion Class
    Dropped ttargetColumnId var since it wasn't actually useful as planned

--- BMA/grails-app/controllers/beaches/IndexController.groovy
+++ BMA/grails-app/controllers/beaches/IndexController.groovy
@@ -162,9 +162,9 @@ class IndexController {
                         new TextQuestion(columnId: 'WIND_SPEED', prompt: 'Wind speed (MPH)', errorm:"Must be a nonnegative integer", type:"numeric", pattern:"(0*[1-9][0-9]*)|0*", step:0.0001),
                         //@TODO find value of wind speed units
                         new HiddenQuestion(columnId: 'WIND_SPEED_UNITS', value: 'MPH', keep: true),
-                        new CorrelatedTextQuestion(columnId: 'WIND_DIR_DEGREES', columnId2: 'WIND_DIR_DESC', initValue: 'Calm', prompt: 'Wind direction in degrees', errorm:"Must be an integer between 0 and 360 (inclusive)", type:"numeric", pattern: "(0*360)|(0*3[0-5][0-9])|(0*[1-2][0-9][0-9])|(0*[1-9][0-9])|(0*[1-9])|0*", step:0.01, onchange: 'AlterWindDirDesc()'),
+                        new CorrelatedTextQuestion(columnId: 'WIND_DIR_DEGREES', columnId2: 'WIND_DIR_OUTPUT', targetColumnId: 'WIND_DIR_DESC', initValue: 'Calm', prompt: 'Wind direction in degrees', errorm:"Must be an integer between 0 and 360 (inclusive)", type:"numeric", pattern: "(0*360)|(0*3[0-5][0-9])|(0*[1-2][0-9][0-9])|(0*[1-9][0-9])|(0*[1-9])|0*", step:0.01, onchange: 'AlterWindDirDesc()', oninput: 'AlterWindDirDesc()'),
                         //new HiddenQuestion(columnId: 'WIND_DIR_DESC', value: 'Calm', keep: true ),
-                        
+
 
                         // Weather
                         new SelectQuestion(columnId: 'WEATHER_DESC', options: [
@@ -268,7 +268,7 @@ class IndexController {
                                 new Tuple2('Light Green ', false),
                         ], hasTitle:true,title:"Algae color:"),
                         new CheckQuestion(columnId: 'ALGAE_COLOR_BRGHT_GREEN', prompts: [ // Bright is intentionally spelled like this
-                                new Tuple2('Bright Green', false),
+                                                                                          new Tuple2('Bright Green', false),
                         ]),
                         new CheckQuestion(columnId: 'ALGAE_COLOR_DRK_GREEN', prompts: [
                                 new Tuple2('Dark Green', false),
@@ -344,9 +344,20 @@ class TextQuestion extends Question {
     String errorm = "Invalid input" // Message written under field when input is invalid
 }
 
-class CorrelatedTextQuestion extends TextQuestion {
+class CorrelatedTextQuestion extends Question {
     String columnId2
-    String initValue = ""
+    //String targetColumnId
+    String initValue = "~"
+
+    String prompt
+    String type = "text"
+    String pattern = ".*"
+    String step
+    String list = ""
+    String onchange = ""
+    String oninput = ""
+    String errorm = "Invalid input" // Message written under field when input is invalid
+
 }
 
 class CheckQuestion extends Question {
